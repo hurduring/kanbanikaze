f8951fefab61771ea772ce62435a0e44648d3ced
hurduring <idontwanttogoonthecart@gmail.com>
2016-02-05 13:55:25 +0300

    adding dragging indication for object which we drag

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -25,19 +25,24 @@ const noteTarget = {
     }
 };
 
-@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
-    connectDragSource: connect.dragSource()
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect, monitor) => ({
+    connectDragSource: connect.dragSource(),
+    isDragging: monitor.isDragging() // map isDragging() state to isDragging prop
 }))
 @DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
     connectDropTarget: connect.dropTarget()
 }))
 export default class Note extends React.Component {
+
     render() {
-        const {connectDragSource, connectDropTarget,
-            id, onMove, ...props} = this.props;
+        const {connectDragSource, connectDropTarget, isDragging,
+            onMove, id, ...props} = this.props;
 
         return connectDragSource(connectDropTarget(
-            <li {...props}>{props.children}</li>
+            <li style={{
+        opacity: isDragging ? 0 : 1
+      }} {...props}>{props.children}</li>
         ));
     }
 }
