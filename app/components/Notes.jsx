d8d38a5da79f8f8e683b85b626fcdc1191bc4b8d
hurduring <idontwanttogoonthecart@gmail.com>
2016-02-08 14:09:42 +0300

    fixing bug when you could dnd note while editing

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -8,7 +8,9 @@ export default ({notes, onValueClick, onEdit, onDelete}) => {
     return (
         <ul className="notes">{notes.map(note =>
 
-            <Note className="note" id={note.id} key={note.id} onMove={LaneActions.move}>
+            <Note className="note" id={note.id} key={note.id}
+                  editing={note.editing} onMove={LaneActions.move}>
+
                 <Editable
                     editing={note.editing}
                     value={note.task}
